36838651b9def15623ae0c12ae8ca85fa0e96a19
Lcbx <luc.courbariaux@gmail.com>
2025-04-07 23:41:48 +0200

    refactoring, checked we're at least faster than dictionary implementation

--- sources/ecs.swift
+++ sources/ecs.swift
@@ -1,96 +1,147 @@
 typealias Entity = Int
-typealias TypeId = ObjectIdentifier
 typealias ComponentId = Int
 typealias ComponentMask = Int
 
-typealias ComponentEntry<T> = (entity:Entity, component:T)
 
-// TODO: make ComponentSet a SoA so we don't retrieve the entityId whenever we iterate it
-typealias ComponentSet<T> = [ComponentEntry<T>]
+protocol Component {
+    static var componentId : ComponentId {get set}
+}
+
+typealias ComponentEntry<T : Component> = (entity:Entity, component:T)
+
+protocol ComponentStorage {
+    func setEntityCount(_ count :Int)
+}
+
+class ComponentSet<T : Component> : ComponentStorage {
+    public var sparse = [Entity]()
+    public var dense = [ComponentEntry<T>]()
+
+    public init(){
+        dense.reserveCapacity(512)
+    }
+
+    public func add(_ ce : ComponentEntry<T> ){
+        sparse[ce.entity] = dense.count
+        dense.append(ce)
+    }
+
+    public func setEntityCount(_ count :Int){
+        if count < sparse.count { return }
+        sparse.reserveCapacity(count)
+        sparse += [Entity](repeating:-1, count: count - sparse.count)
+    }
+
+    public func set(_ ce : ComponentEntry<T> ){
+        let denseIndex = sparse[ce.entity]
+        guard denseIndex != -1 else { add(ce); return }
+        dense[denseIndex] = ce
+    }
+
+    public func get(_ entity :Entity ) -> T? {
+        let denseIndex = sparse[entity]
+        guard denseIndex != -1 else { return nil }
+        return dense[denseIndex].component
+    }
+
+
+    public func getAsKeyValues() -> LazySequence<[(entity: Entity, component: T)]> {
+        return dense.lazy
+    }
+}
+
+
+// even slower, that's worth something I guess 
+
+// class ComponentSet<T: Component>: ComponentStorage {
+//     private var storage: [Entity: T] = [:]
+
+//     public init() {
+//         storage.reserveCapacity(512)
+//     }
+
+//     public func add(_ ce: ComponentEntry<T>) {
+//         storage[ce.entity] = ce.component
+//     }
+
+//     public func setEntityCount(_ count: Int) {
+//         // Not needed when using a dictionary
+//     }
+
+//     public func set(_ ce: ComponentEntry<T>) {
+//         storage[ce.entity] = ce.component
+//     }
+
+//     public func get(_ entity: Entity) -> T? {
+//         return storage[entity]
+//     }
+
+//     public func getAsKeyValues() -> LazySequence<LazyMapSequence<LazySequence<[Entity : T]>.Elements, (entity: Entity, component: T)>.Elements> {
+//         return storage.lazy.map({ (key, value) in return (entity:key, component:value) }).lazy
+//     }
+// }
 
 // NOTE: maybe use ComponentMask max value to mark deleted components
 class EntityComponentSystem {
 
-    var componentTypeToStorage: [TypeId] = []
     var entityHasComponent : [ComponentMask] = []
-    var storages: [Any] = []
+    var storages: [ComponentStorage] = []
 
     public var entityCount: Entity = 0
 
     public init(){}
 
     public func CreateEntities(_ n: Int = 1){
+        if n < 1 { return }
         entityCount += n
         if n == 1 { entityHasComponent.append(0); return }
         entityHasComponent += [ComponentMask](repeating: 0, count: n)
+        for s in storages { s.setEntityCount(entityCount) }
     }
 
-    func getComponentId_impl(_ type:TypeId) -> ComponentId? { return componentTypeToStorage.firstIndex(of:type); }
-
-    func getStorage<T>() -> ComponentSet<T> {
-        let id = getComponentId_impl(TypeId(T.self))
-        return storages[id!] as! ComponentSet<T>
+    func getStorage<T : Component>() -> ComponentSet<T> {
+        return storages[T.self.componentId] as! ComponentSet<T>
     }
 
-    public func getComponentId<T>(_ type: T.Type, capacity:Int=64) -> ComponentId {
-        let typeId = TypeId(type)
-        if let id = getComponentId_impl(typeId) { return id }
-        componentTypeToStorage.append(typeId)
-        var storage = ComponentSet<T>()
-        storage.reserveCapacity(capacity)
-        storages.append(storage)
-        return componentTypeToStorage.count-1
+    public func addComponentType<T : Component>(_ type: T.Type) {
+        if type.componentId == -1 {
+            type.componentId = storages.count
+            let storage = ComponentSet<T>()
+            storage.setEntityCount(entityCount)
+            storages.append(storage)
+        }
     }
     
-    public func addComponent<T>(_ component : T, entity : Entity) {
-        let id = getComponentId_impl(TypeId(T.self))!
-        addComponentFast(component, componentId:id, entity:entity)
-    }
-
-    public func addComponentFast<T>(_ component : T, componentId : ComponentId, entity : Entity) {
-        var storage : ComponentSet<T> = storages[componentId] as! ComponentSet<T>
+    public func addComponent<T : Component>(_ component : T, entity : Entity) {
+        let componentId = T.self.componentId
         entityHasComponent[entity] |= 1 << componentId
-        storage.append((entity, component))
+        let storage = storages[componentId] as! ComponentSet<T>
+        storage.add((entity, component))
         storages[componentId] = storage
     }
 
-    public func componentMask<T>(_ type : T.Type) -> ComponentMask {
-        let id = getComponentId_impl(TypeId(T.self))!
-        return 1 << id
-    }
-
-    public func componentMask(_ types : [TypeId] ) -> ComponentMask{
-        var mask : ComponentMask = 0;
-        for t in types{
-            let id = getComponentId_impl(t)!
-            mask |= 1 << id
-        }
-        return mask
+    public func componentMask<T : Component>(_ type : T.Type) -> ComponentMask {
+        return 1 << type.componentId
     }
 
     public func hasComponents(_ entity : Entity, _ mask : ComponentMask ) -> Bool{
         return entityHasComponent[entity] & mask != 0
     }
 
-    // need to keep storage sorted by entities if we want multi-component iterator to work 
-    public func Sort<T>(_ type : T.Type) {
-        var t :ComponentSet<T> = getStorage()
-        t.sort(by: { return $0.entity > $1.entity })
-    }
-
-    public func ForEach<T>(_ typeT : T.Type) -> ComponentSet<T> {
+    public func ForEach<T : Component>(_ typeT : T.Type) -> ComponentSet<T> {
         let t :ComponentSet<T> = getStorage()
         return t
     }
 
-    public func ForEach<T, U>(_ typeT : T.Type, _ typeU : U.Type)
-    -> LazyMapSequence<LazySequence<Zip2Sequence<ComponentSet<T>, [ComponentEntry<U>]>>.Elements, (Entity, T, U)> {
+    public func ForEach<T : Component, U : Component>(_ typeT : T.Type, _ typeU : U.Type)
+    -> LazyMapSequence<LazySequence<Zip2Sequence<LazyFilterSequence<LazySequence<[(entity: Entity, component: T)]>.Elements>, LazyFilterSequence<LazySequence<[(entity: Entity, component: U)]>.Elements>>>.Elements, (Entity, T, U)>
+    {
         let t :ComponentSet<T> = getStorage()
         let u :ComponentSet<U> = getStorage()
-        let mask = componentMask( [TypeId(typeT), TypeId(typeU)] )
+        let mask = componentMask(T.self) | componentMask(U.self)
         return zip(
-            t.filter { return self.hasComponents($0.entity, mask) },
-            u.filter { return self.hasComponents($0.entity, mask) }
+            t.getAsKeyValues().filter { return self.hasComponents($0.entity, mask) },
+            u.getAsKeyValues().filter { return self.hasComponents($0.entity, mask) }
         ).lazy.map({ return ($0.entity, $0.component, $1.component) })
     }
 
--- sources/main.swift
+++ sources/main.swift
@@ -3,14 +3,12 @@ import raylib
 
 
 
-typealias Color = raylib.Color
 typealias Vec2 = raylib.Vector2
-
 let rnd_uint8 = { return UInt8(raylib.GetRandomValue(0, 255)) }
-let rnd_color = { return Color(r: rnd_uint8(), g: rnd_uint8(), b: rnd_uint8(), a: 255) }
+let rnd_color = { return raylib.Color(r: rnd_uint8(), g: rnd_uint8(), b: rnd_uint8(), a: 255) }
 
-let RAYWHITE = Color(r:255,g:255,b:255,a:255)
-let LIGHTGRAY = Color(r:100,g:0,b:100,a:255)
+let RAYWHITE = raylib.Color(r:255,g:255,b:255,a:255)
+let LIGHTGRAY = raylib.Color(r:100,g:0,b:100,a:255)
 
 let WINDOW_SIZE = Vec2(x:800, y:400)
 
@@ -23,36 +21,42 @@ raylib.SetTargetFPS(60)
 
 let SQUARE_N = 30000
 
-struct Position {
-//class Position {
-//    public init(pos:Vec2){self.pos = pos}
-    public var pos : Vec2
+struct Position : Component {
+    static var componentId = -1
+    public var x : Float
+    public var y : Float
 }
-struct Velocity {
-//class Velocity {
-//    public init(vel:Vec2){self.vel = vel}
-    public var vel : Vec2
+struct Velocity : Component {
+    static var componentId = -1
+    public var x : Float
+    public var y : Float
+}
+
+struct Color : Component{
+    static var componentId = -1
+    public var data : raylib.Color
 }
 
 var ecs = EntityComponentSystem();
-let posId = ecs.getComponentId(Position.self)
-let velId = ecs.getComponentId(Velocity.self)
-let colId = ecs.getComponentId(Color.self)
-ecs.CreateEntities(SQUARE_N)
+ecs.addComponentType(Position.self)
+ecs.addComponentType(Velocity.self)
+ecs.addComponentType(Color.self)
+ecs.CreateEntities(SQUARE_N/2)
+ecs.CreateEntities(SQUARE_N/2)
 
-typealias Square = (pos : Vec2, vel: Vec2, color: Color)
+typealias Square = (pos : Vec2, vel: Vec2, color: raylib.Color)
 var squares = [Square]()
 
 for i in 0..<SQUARE_N{
     
-    // squares.append(Square(
-    //    pos:Vec2(x:Float(i)*WINDOW_SIZE.x/Float(SQUARE_N),y:WINDOW_SIZE.y/Float(2)),
-    //    vel:Vec2(x:Float(rnd_uint8())/255.0,y:Float(rnd_uint8())/255.0),
-    //    color:rnd_color()))
+     squares.append(Square(
+        pos:Vec2(x:Float(i)*WINDOW_SIZE.x/Float(SQUARE_N),y:WINDOW_SIZE.y/Float(2)),
+        vel:Vec2(x:Float(rnd_uint8())/255.0,y:Float(rnd_uint8())/255.0),
+        color:rnd_color()))
     
-    ecs.addComponentFast(Position(pos: Vec2(x:Float(i)*WINDOW_SIZE.x/Float(SQUARE_N),y:WINDOW_SIZE.y/Float(2))), componentId:posId, entity:i)
-    ecs.addComponentFast(Velocity(vel: Vec2(x:Float(rnd_uint8())/255.0,y:Float(rnd_uint8())/255.0)), componentId:velId, entity:i)
-    ecs.addComponentFast(rnd_color(), componentId:colId, entity:i)
+    ecs.addComponent(Position(x:Float(i)*WINDOW_SIZE.x/Float(SQUARE_N),y:WINDOW_SIZE.y/Float(2)), entity:i)
+    ecs.addComponent(Velocity(x:Float(rnd_uint8())/255.0,y:Float(rnd_uint8())/255.0), entity:i)
+    ecs.addComponent(Color(data:rnd_color()), entity:i)
 }
 
 while !raylib.WindowShouldClose()
@@ -63,44 +67,35 @@ while !raylib.WindowShouldClose()
         raylib.DrawText("\(raylib.GetFPS())", 10, 10, 20, LIGHTGRAY)
 
         for (_, pos, col) in ecs.ForEach(Position.self, Color.self) {
-            DrawRectangleV(pos.pos, Vec2(x:10,y:10), col);
+           DrawRectangle(Int32(pos.x), Int32(pos.y), 10, 10, col.data);
         }
 
         // TODO: find a better way to edit components
         // maybe make ForEach return the index in storage since that's nore relevant than entity's id
-        var positions = ecs.ForEach(Position.self)
-        var velocities = ecs.ForEach(Velocity.self)
-        var i = 0
-        var j = 0
+        let positions = ecs.ForEach(Position.self)
+        let velocities = ecs.ForEach(Velocity.self)
         for (entity, var pos, var vel) in ecs.ForEach(Position.self, Velocity.self) {
-            while positions[i].entity != entity{
-                i+=1
-            }
-            while velocities[j].entity != entity{
-                j+=1
-            }
-            pos.pos += vel.vel
+           pos.x += vel.x
+           pos.y += vel.y
+           positions.set((entity, pos))
 
-            if pos.pos.x < 0 { pos.pos.x = 0; vel.vel.x = -vel.vel.x }
-            if pos.pos.y < 0 { pos.pos.y = 0; vel.vel.y = -vel.vel.y }
-            if pos.pos.x > WINDOW_SIZE.x { pos.pos.x = WINDOW_SIZE.x; vel.vel.x = -vel.vel.x }
-            if pos.pos.y > WINDOW_SIZE.y { pos.pos.y = WINDOW_SIZE.y; vel.vel.y = -vel.vel.y }
+           if pos.x < 0 { pos.x = 0; vel.x = -vel.x }
+           if pos.y < 0 { pos.y = 0; vel.y = -vel.y }
+           if pos.x > WINDOW_SIZE.x { pos.x = WINDOW_SIZE.x; vel.x = -vel.x }
+           if pos.y > WINDOW_SIZE.y { pos.y = WINDOW_SIZE.y; vel.y = -vel.y }
             
-            positions[i] = (entity, pos)
-            velocities[j] = (entity, vel)
+           velocities.set((entity, vel))
         }
-        ecs.storages[posId] = positions
-        ecs.storages[velId] = velocities
 
-        // for (i, var square) in squares.enumerated(){
-        //    DrawRectangleV(square.pos, Vec2(x:10,y:10), square.color);
-        //    square.pos += square.vel
-        //    if square.pos.x < 0 { square.pos.x = 0; square.vel.x = -square.vel.x }
-        //    if square.pos.y < 0 { square.pos.y = 0; square.vel.y = -square.vel.y }
-        //    if square.pos.x > WINDOW_SIZE.x { square.pos.x = WINDOW_SIZE.x; square.vel.x = -square.vel.x }
-        //    if square.pos.y > WINDOW_SIZE.y { square.pos.y = WINDOW_SIZE.y; square.vel.y = -square.vel.y }
-        //    squares[i] = square
-        // }
+          // for (i, var square) in squares.enumerated(){
+             // DrawRectangleV(square.pos, Vec2(x:10,y:10), square.color);
+             // square.pos += square.vel
+             // if square.pos.x < 0 { square.pos.x = 0; square.vel.x = -square.vel.x }
+             // if square.pos.y < 0 { square.pos.y = 0; square.vel.y = -square.vel.y }
+             // if square.pos.x > WINDOW_SIZE.x { square.pos.x = WINDOW_SIZE.x; square.vel.x = -square.vel.x }
+             // if square.pos.y > WINDOW_SIZE.y { square.pos.y = WINDOW_SIZE.y; square.vel.y = -square.vel.y }
+             // squares[i] = square
+          // }
 
 
     raylib.EndDrawing()
