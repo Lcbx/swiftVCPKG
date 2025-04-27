296718873faab91a321d3d0516030d27394eb50a
Lcbx <luc.courbariaux@gmail.com>
2025-04-27 21:44:05 +0100

    build scripts : add requirements note, cleanup, suppress c compilation warnings

--- full_build.sh
+++ full_build.sh
@@ -1,12 +1,18 @@
 #!bash
 
+# requirements :
+# - swift language sdk
+# - CMAKE build system
+# - ninja build system
+# - vcpkg (dont forget to bootstrap)
+# --- by default vcpkg dir is expected to be in same parent dir
+
 export VCPKG_ROOT=$(ls -t -U | readlink -f ../vcpkg/)
 export VCPKG_DEFAULT_TRIPLET=x64-windows
 export VCPKG_TARGET_TRIPLET=x64-windows
 PATH=$VCPKG_ROOT:$PATH
-vcpkg install raylib >/dev/null
-cmake  -G 'Ninja' -S . -B build #>/dev/null #-DCMAKE_BUILD_TYPE=Debug
-#-DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake"
-cd build 
+vcpkg install raylib
+cmake  -G 'Ninja' -S . -B build #-DCMAKE_BUILD_TYPE=Debug
 
+cd build 
 ninja && ./swiftEngine.exe
